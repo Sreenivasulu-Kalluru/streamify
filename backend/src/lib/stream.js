2fee8ca8631a55fb33fff37f74ada97804008c8b
Vasu Kalluru <84690382+KALLURUSREENIVASULU@users.noreply.github.com>
2025-05-13 11:27:13 +0530

    Done with chat route and getStreamToken functionality and even the whole backend code

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -19,5 +19,12 @@ export const upsertStreamUser = async (userData) => {
   }
 };
 
-// todo: do it later
-export const genereateStreamToken = (userId) => {};
+export const generateStreamToken = (userId) => {
+  try {
+    // ensure userId is a string
+    const userIdStr = userId.toString();
+    return streamClient.createToken(userIdStr);
+  } catch (error) {
+    console.error('Error generating Stream token:', error);
+  }
+};
