d2bfa8bf54e29a5ca9de1f3f70c95367543a3dbc
Vasu Kalluru <84690382+KALLURUSREENIVASULU@users.noreply.github.com>
2025-05-19 10:53:52 +0530

    fixes

--- frontend/src/components/UserProfileCard.jsx
+++ frontend/src/components/UserProfileCard.jsx
@@ -21,23 +21,25 @@ const UserProfileCard = () => {
   }, []);
 
   return (
-    <div className="relative inline-block text-left">
-      {/* Trigger button */}
-      <button
-        onClick={() => setIsOpen(!isOpen)}
-        className="flex items-center p-2 space-x-2 transition rounded-full"
-      >
-        <img
-          src={authUser?.profilePic}
-          alt={authUser?.fullName}
-          className="w-10 h-10 rounded-full"
-        />
-      </button>
+    <>
+      <div className="relative inline-block text-left">
+        {/* Trigger button */}
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          className="flex items-center p-2 space-x-2 transition rounded-full"
+        >
+          <img
+            src={authUser?.profilePic}
+            alt={authUser?.fullName}
+            className="w-10 h-10 rounded-full"
+          />
+        </button>
+      </div>
 
       {/* Profile Card */}
       {isOpen && (
-        <div className="transition-shadow card bg-base-200 hover:shadow-md">
-          <div className="z-10 p-4 mt-12 card-body">
+        <div className="transition-shadow mt-14 card bg-base-200 hover:shadow-md">
+          <div className="z-10 p-4 card-body">
             {/* USER INFO */}
             <div className="flex items-center gap-3 mb-3">
               <div className="avatar size-12">
@@ -63,7 +65,7 @@ const UserProfileCard = () => {
           </div>
         </div>
       )}
-    </div>
+    </>
   );
 };
 
