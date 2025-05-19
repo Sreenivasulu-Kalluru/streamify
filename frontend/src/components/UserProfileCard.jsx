fb98d1096f2ae2dc560fc095df8531a448d62088
Vasu Kalluru <84690382+KALLURUSREENIVASULU@users.noreply.github.com>
2025-05-19 12:16:49 +0530

    btn fix

--- frontend/src/components/UserProfileCard.jsx
+++ frontend/src/components/UserProfileCard.jsx
@@ -26,7 +26,7 @@ const UserProfileCard = () => {
         {/* Trigger button */}
         <button
           onClick={() => setIsOpen(!isOpen)}
-          className="flex items-center p-2 space-x-2 transition-all rounded-full focus:ring-2 ring-[#353b42]"
+          className="flex items-center p-2 space-x-2 transition-all rounded-full btn btn-ghost btn-circle"
         >
           <img
             src={authUser?.profilePic}
@@ -39,7 +39,7 @@ const UserProfileCard = () => {
       {/* Profile Card */}
       {isOpen && (
         <div
-          className="absolute right-0 z-50 w-64 transition-all bg-base-200 border border-gray-300 mt-[250px] mr-[85px] shadow-xl rounded-2xl hover:shadow-xl"
+          className="absolute right-0 z-50 w-64 transition-all bg-base-200 border border-[#353b42] mt-[250px] mr-[85px] shadow-xl rounded-2xl hover:shadow-xl"
           ref={cardRef}
         >
           <div className="z-10 p-4 card-body">
